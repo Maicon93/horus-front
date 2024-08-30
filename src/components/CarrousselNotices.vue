b35a9498a8a82d3cf66f7e8664f29ddda00143d9
Maicon Ramos <informatica5@zagonel.com.br>
2024-08-29 21:04:03 -0300

    feat: adicionado componentes de carroussel

---
+++ src/components/CarrousselNotices.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-carousel class="bg-black p-10">
+    <v-carousel-item>
+      <div class="flex justify-between items-center space-x-4 mx-20">
+        <div v-for="(item, index) in items" :key="index" class="w-[350px] h-[500px]">
+          <div class="bg-white rounded-lg shadow-lg overflow-hidden h-full">
+            <img :src="item.image" alt="Image" class="w-full h-1/2 object-cover">
+            <div class="p-6 h-1/2">
+              <h3 class="text-sm font-semibold truncate">{{ item.title }}</h3>
+              <p class="text-gray-500 text-xs">{{ item.date }}</p>
+              <p class="text-gray-700 text-xs mt-1 truncate">{{ item.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          image: 'https://static.wixstatic.com/media/d2be83_b605b78313ce44fa9b076bd3cecd0049~mv2.png/v1/crop/x_41,y_0,w_2841,h_1772/fill/w_1029,h_642,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/_Not%C3%ADcias%20%20.png',
+          title: 'EDF : Workshop sobre ciclismo',
+          date: '17 de abril de 2023',
+          description: 'Na noite de ontem, dia 01/04, a turma da 1ª fase de Educação Física...'
+        },
+        {
+          image: 'https://static.wixstatic.com/media/d2be83_b605b78313ce44fa9b076bd3cecd0049~mv2.png/v1/crop/x_41,y_0,w_2841,h_1772/fill/w_1029,h_642,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/_Not%C3%ADcias%20%20.png',
+          title: 'Alunos do curso de Pedagogia exploram a Trilha do Saber',
+          date: '17 de abril de 2023',
+          description: 'Na noite de ontem, dia 01/04, a turma da 1ª fase de Educação Física...'
+        },
+        {
+          image: 'https://static.wixstatic.com/media/d2be83_b605b78313ce44fa9b076bd3cecd0049~mv2.png/v1/crop/x_41,y_0,w_2841,h_1772/fill/w_1029,h_642,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/_Not%C3%ADcias%20%20.png',
+          title: '28ª Maratona de Programação',
+          date: '17 de abril de 2023',
+          description: 'Neste final de semana, a Horus Faculdades representada pelo Professor...'
+        },
+        {
+          image: 'https://static.wixstatic.com/media/d2be83_b605b78313ce44fa9b076bd3cecd0049~mv2.png/v1/crop/x_41,y_0,w_2841,h_1772/fill/w_1029,h_642,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/_Not%C3%ADcias%20%20.png',
+          title: '28ª Maratona de Programação',
+          date: '17 de abril de 2023',
+          description: 'Neste final de semana, a Horus Faculdades representada pelo Professor...'
+        }
+      ]
+    }),
+  }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
-    <v-carousel>
-      <v-carousel-item
-        src="https://static.wixstatic.com/media/d2be83_8a5288b5c5fd46718f49a5f9af0551c2~mv2.jpeg/v1/crop/x_0,y_1,w_1019,h_633/fill/w_1018,h_632,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MBA%20BANNER.jpeg"
-        color="black"
-      ></v-carousel-item>
-
-      <v-carousel-item
-        src="https://static.wixstatic.com/media/d2be83_b605b78313ce44fa9b076bd3cecd0049~mv2.png/v1/crop/x_41,y_0,w_2841,h_1772/fill/w_1029,h_642,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/_Not%C3%ADcias%20%20.png"
-        color="black"
-      ></v-carousel-item>
-
-      <v-carousel-item
-        src="https://static.wixstatic.com/media/d2be83_e2d20fca1f5d4f9887584df6e80a239a~mv2.jpeg/v1/crop/x_2,y_0,w_1015,h_634/fill/w_1015,h_634,al_c,q_85,enc_auto/WhatsApp%20Image%202023-06-01%20at%2010_12_26%20(1).jpeg"
-        color="black"
-      ></v-carousel-item>
-    </v-carousel>
+    <CarrousselInformations />
+    <CarrousselNotices />
   </div>
 </template>
 
 <script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
+  import CarrousselInformations from '@/components/CarrousselInformations.vue'
+  import CarrousselNotices from '@/components/CarrousselNotices.vue'
 </script>
